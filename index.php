6031db7fc0d28245be97febe7bad9ef8df79aefd
EduxMontivero <montivero.x@gmail.com>
2023-10-28 19:04:08 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lectura</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <center>
+        <div class="row">
+            <div class="col-8">
+                <h4>
+                    Lectura lalala
+                </h4>
+            </div>
+            <div class="col-4">
+                Lc.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse tenetur quisquam iste, ipsam quos iusto saepe vel ab aut deserunt officiis adipisci! Culpa unde dolore necessitatibus maxime excepturi fugiat ut?
+            </div>
+        </div>
+    </center>
+</body>
+</html>
